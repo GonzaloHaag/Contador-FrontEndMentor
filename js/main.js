3f1d30308091a551f7d38e62d0a0189cc5d5f1f4
Gonzalo Haag <gonzalohaag13@gmail.com>
2023-03-24 18:11:04 -0300

    Mi primer commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let days = document.querySelector(".days");
+let hours = document.querySelector(".hours");
+let minutes = document.querySelector(".minutes");
+let seconds = document.querySelector(".seconds");
+
+
+
+let fecha = new Date ("jan 1,2023 00:00:00").getTime();
+
+console.log(fecha);
